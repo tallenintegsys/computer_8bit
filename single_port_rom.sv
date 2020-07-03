c84384a7ee1d9d9e85b3bb359e03c5daa4ce2fd0
Tim Allen <tallen@integsys.biz>
2020-07-03 14:36:20 -0400

    this aint synthesizable

--- single_port_rom.sv
+++ single_port_rom.sv
@@ -9,32 +9,17 @@ module single_port_rom
 	output reg [(DATA_WIDTH-1):0] q
 );
 
-	// Declare the ROM variable
-	reg [DATA_WIDTH-1:0] rom[2**ADDR_WIDTH-1:0];
-	int File, dk;
-
-	// Initialize the ROM with $readmemb.  Put the memory contents
-	// in the file single_port_rom_init.txt.  Without this file,
-	// this design will not compile.
-
-	// See Verilog LRM 1364-2001 Section 17.2.8 for details on the
-	// format of this file, or see the "Using $readmemb and $readmemh"
-	// template later in this section.
-
 	initial
 	begin
-		File = $fopen("rom.bin", "rb");
-		dk = $fread(rom, File, 16'hffff - 20479);
 		q <= 8'hzz;
 	end
 
 	always @ (posedge clk)
 	begin
-		if (!ce) begin
-			q <= 8'hzz;
-		end else begin
-			q <= rom[addr];
-		end
+		case (addr)
+			`include "rom_bin.v"
+		default: q <= 8'hea;
+		endcase
 	end
 
 endmodule
