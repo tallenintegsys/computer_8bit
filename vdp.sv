238a57d0073686c2e21f55b6b884b58b6506fe4b
tallenintegsys <tallen@integsys.biz>
2024-03-15 16:06:14 -0400

    improve readability

--- vdp.sv
+++ vdp.sv
@@ -13,64 +13,64 @@ $700  06 $700-727  14 $728-74F  22 $750-777  $778-77F
 $780  07 $780-7A7  15 $7A8-7CF  23 $7D0-7F7  $7F8-7FF */
 
 module vdp (
-    input           clock_50,
-    input           clk,
-    input           res,
-    output  logic   [7:0]vga_b,
-    output  logic   vga_blank_n,    // to D2A chip, active low
-    output  logic   vga_clk,        // latch the RGBs and put 'em on the DACs
-    output  logic   [7:0]vga_g,
-    output  logic   vga_hs,         // DB19 pin, active low
-    output  logic   [7:0]vga_r,
-    output  logic   vga_sync_n,     // to D2A chip, active low
-    output  logic   vga_vs,         // DB19 pin, active low
-    output  logic   [15:0]txt_adr,      // XXX for now we reach out
-    input           [7:0]txt_q);
+    input               clock_50,
+    input               clk,
+    input               res,
+    output logic [7:0]  vga_b,          // to D2A chip
+    output logic        vga_blank_n,    // to D2A chip, active low
+    output logic        vga_clk,        // latch the RGBs and put 'em on the DACs
+    output logic [7:0]  vga_g,          // to D2A chip
+    output logic        vga_hs,         // DB19 pin, active low
+    output logic [7:0]  vga_r,          // to D2A chip
+    output logic        vga_sync_n,     // to D2A chip, active low
+    output logic        vga_vs,         // DB19 pin, active low
+    output logic [15:0] txt_adr,        // XXX for now we reach out
+    input [7:0]         txt_q);
 
-logic   [23:0] vram_d;
-logic   [15:0] vram_wadr;
-logic   [23:0] vram_q;
-logic   [15:0] vram_radr;
-logic   [7:0] crom_q;
-logic   [2:0] x7;
-logic   [5:0] x40;
-logic   [3:0] y8;
-logic   [4:0] y24;
+logic [23:0]    vram_d;
+logic [15:0]    vram_wadr;
+logic [23:0]    vram_q;
+logic [15:0]    vram_radr;
+logic [7:0]     crom_q;
+logic [2:0]     x7;
+logic [5:0]     x40;
+logic [3:0]     y8;
+logic [4:0]     y24;
 
 vram #(24,16) vram (
-    .d              (vram_d),
-    .w_adr          (vram_wadr),
-    .we             (1'd1),
-    .r_clk          (clock_50),
-    .w_clk          (clock_50),
-    .q              (vram_q),
-    .r_adr          (vram_radr));
+    .d      (vram_d),
+    .w_adr  (vram_wadr),
+    .we     (1'd1),
+    .r_clk  (clock_50),
+    .w_clk  (clock_50),
+    .q      (vram_q),
+    .r_adr  (vram_radr));
 
 crom #(8,11) crom (
-    .adr            ({txt_q[7:0], y8[2:0]}),
-    .clk            (clock_50),
-    .q              (crom_q));
+    .adr    ({txt_q[7:0], y8[2:0]}),
+    .clk    (clock_50),
+    .q      (crom_q));
 
 vga vga (
     .clock_50,
     .d              (vram_q),
     .adr            (vram_radr),
-    .vga_b          (vga_b),
-    .vga_blank_n    (vga_blank_n),        // to D2A chip, active low
-    .vga_clk        (vga_clk),            // latch the RGBs and put 'em on the DACs
-    .vga_g          (vga_g),
-    .vga_hs         (vga_hs),             // DB19 pin, active low
-    .vga_r          (vga_r),
-    .vga_sync_n     (vga_sync_n),         // to D2A chip, active low
-    .vga_vs         (vga_vs));            // DB19 pin, active low
+    .vga_b          (vga_b),        // to D2A chip
+    .vga_blank_n    (vga_blank_n),  // to D2A chip, active low
+    .vga_clk        (vga_clk),      // latch the RGBs and put 'em on the DACs
+    .vga_g          (vga_g),        // to D2A chip
+    .vga_hs         (vga_hs),       // DB19 pin, active low
+    .vga_r          (vga_r),        // to D2A chip
+    .vga_sync_n     (vga_sync_n),   // to D2A chip, active low
+    .vga_vs         (vga_vs));      // DB19 pin, active low
 
 always @ (posedge clk, negedge res) begin
     if (!res) begin
         vram_wadr = 0; //last pixel
         txt_adr = 16'h400;
-        x7 = 0;
+        x7  = 0;
         x40 = 0;
-        y8 = 0;
+        y8  = 0;
         y24 = 0;
     end else begin
         x7++;
@@ -83,33 +83,33 @@ always @ (posedge clk, negedge res) begin
             x40 = 0;
             y8++;
             if (y8 != 4'd8) begin
-            case (txt_adr)
-                16'h428 : txt_adr = 16'h400;
-                16'h4a8 : txt_adr = 16'h480;
-                16'h528 : txt_adr = 16'h500;
-                16'h5a8 : txt_adr = 16'h580;
-                16'h628 : txt_adr = 16'h600;
-                16'h6a8 : txt_adr = 16'h680;
-                16'h728 : txt_adr = 16'h700;
-                16'h7a8 : txt_adr = 16'h780;
-                16'h450 : txt_adr = 16'h428;
-                16'h4d0 : txt_adr = 16'h4a8;
-                16'h550 : txt_adr = 16'h528;
-                16'h5d0 : txt_adr = 16'h5a8;
-                16'h650 : txt_adr = 16'h628;
-                16'h6d0 : txt_adr = 16'h6a8;
-                16'h750 : txt_adr = 16'h728;
-                16'h7d0 : txt_adr = 16'h7a8;
-                16'h478 : txt_adr = 16'h450;
-                16'h4f8 : txt_adr = 16'h4d0;
-                16'h578 : txt_adr = 16'h550;
-                16'h5f8 : txt_adr = 16'h5d0;
-                16'h678 : txt_adr = 16'h650;
-                16'h6f8 : txt_adr = 16'h6d0;
-                16'h778 : txt_adr = 16'h750;
-                16'h7f8 : txt_adr = 16'h7d0;
-                default: ; //no nothing
-            endcase
+                case (txt_adr)
+                    16'h428 : txt_adr = 16'h400;
+                    16'h4a8 : txt_adr = 16'h480;
+                    16'h528 : txt_adr = 16'h500;
+                    16'h5a8 : txt_adr = 16'h580;
+                    16'h628 : txt_adr = 16'h600;
+                    16'h6a8 : txt_adr = 16'h680;
+                    16'h728 : txt_adr = 16'h700;
+                    16'h7a8 : txt_adr = 16'h780;
+                    16'h450 : txt_adr = 16'h428;
+                    16'h4d0 : txt_adr = 16'h4a8;
+                    16'h550 : txt_adr = 16'h528;
+                    16'h5d0 : txt_adr = 16'h5a8;
+                    16'h650 : txt_adr = 16'h628;
+                    16'h6d0 : txt_adr = 16'h6a8;
+                    16'h750 : txt_adr = 16'h728;
+                    16'h7d0 : txt_adr = 16'h7a8;
+                    16'h478 : txt_adr = 16'h450;
+                    16'h4f8 : txt_adr = 16'h4d0;
+                    16'h578 : txt_adr = 16'h550;
+                    16'h5f8 : txt_adr = 16'h5d0;
+                    16'h678 : txt_adr = 16'h650;
+                    16'h6f8 : txt_adr = 16'h6d0;
+                    16'h778 : txt_adr = 16'h750;
+                    16'h7f8 : txt_adr = 16'h7d0;
+                    default: ; //no nothing
+                endcase
             end
         end
         if (y8 == 4'd8) begin
@@ -146,7 +146,6 @@ always @ (posedge clk, negedge res) begin
         if (y24 == 5'd24) begin
             y24 = 0;
         end
-
         vram_wadr++;
         if (vram_wadr == 280 * 192)
             vram_wadr = 0;
--- vga.sv
+++ vga.sv
@@ -1,94 +1,97 @@
 `timescale 10ns/10ps
 
 module vga (
-    input clock_50,
-    input [23:0] d,
+    input               clock_50,
+    input [23:0]        d,
     output logic [15:0] adr,
-    output logic [7:0]vga_b,
-    output logic vga_blank_n, // to D2A chip, active low
-    output logic vga_clk, // latch the RGBs and put 'em on the DACs
-    output logic [7:0]vga_g,
-    output logic vga_hs, // DB19 pin, active low
-    output logic [7:0]vga_r,
-    output logic vga_sync_n, // to D2A chip, active low
-    output logic vga_vs); // DB19 pin, active low
+    output logic [7:0]  vga_b,          // to D2A chip
+    output logic        vga_blank_n,    // to D2A chip, active low
+    output logic        vga_clk,        // latch the RGBs and put 'em on the DACs
+    output logic [7:0]  vga_g,          // to D2A chip
+    output logic        vga_hs,         // DB19 pin, active low
+    output logic [7:0]  vga_r,          // to D2A chip
+    output logic        vga_sync_n,     // to D2A chip, active low
+    output logic        vga_vs);        // DB19 pin, active low
 
-logic [9:0] h_counter; //visible + blanking
-logic [9:0] v_counter; //visible + blanking
-logic h_blank, v_blank;
-logic v_advance;
-logic reset;
-logic [3:0]reset_counter;
+logic [9:0] h_counter; // visible + blanking
+logic [9:0] v_counter; // visible + blanking
+logic       h_blank;
+logic       v_blank;
+logic       v_advance;
+logic       reset;
+logic [3:0] reset_counter;
 
 //640x480, 60Hz 25.175  640 16  96  48  480 11  2   31
 
 assign vga_blank_n = h_blank & v_blank;
-initial reset = 0;
-initial reset_counter = 0;
-initial vga_clk = 0;
+initial begin
+    reset = 0;
+    reset_counter = 0;
+    vga_clk = 0;
+end
 
 always @ (posedge clock_50) begin
     reset_counter++;
     if (reset_counter == 4'd5)
         reset <= 1;
-    vga_clk <= ~vga_clk; //25MHz
+    vga_clk <= ~vga_clk; // 25MHz
 end
 
 always @ (posedge vga_clk) begin
-if (reset == 0) begin //active low reset
-    h_counter <= 0;
-    v_counter <= 0;
-    vga_hs <= 1'd1;
-    vga_vs <= 1'd1;
-    vga_sync_n <= 0; //no sync on green
-    h_blank <= 0;
-    v_blank <= 0;
-    adr <= 0;
-    v_advance <= 0;
-end else begin
-    h_counter <= h_counter + 10'd1;
-    case (h_counter)
-    000: h_blank <= 0; //blank
-    039: h_blank <= 1; //unblank
-    599: h_blank <= 0; //blank
-    639: h_blank <= 0; //hfront porch start
-    655: begin //hfront porch end
-        vga_hs <= 0; //hsync start
-        v_advance <= ~v_advance;
-        if (!v_counter[0] && v_blank)
-            adr <= adr - 16'd280; //redo the line (line double)
-    end
-    751: vga_hs <= 1'd1; //hback porch start
-    800: begin //hback porch end
-        h_counter <= 0;
-        v_counter <= v_counter + 1;
-    end
-    endcase
-
-    if ((h_blank & v_blank))
-        if (!h_counter[0])
-            adr <= adr + 1'd1;
-
-    case (v_counter)
-    000: v_blank <= 0; //blank
-    048: v_blank <= 1; //unblank
-    432: v_blank <= 0; //blank
-    480: begin //vfront porch start
-        v_blank <= 0; //disable RGB DACs
-        adr <= 0;
-    end
-    491: vga_vs <= 0; //vfront porch end//vsync start
-    493: vga_vs <= 1'd1; //vsync pulse end//vsync end
-    524: begin //vback porch end
-        v_blank <= 1'd1; //enable RGB DACs
-        v_counter <= 0;
-        v_advance <= 0;
-    end
-    endcase
-
-    vga_r <= d[23:16];
-    vga_g <= d[15:8];
-    vga_b <= d[7:0];
-end //if reset
-end //always
+    if (reset == 0) begin   // active low reset
+        h_counter   <= 0;
+        v_counter   <= 0;
+        vga_hs      <= 1'd1;
+        vga_vs      <= 1'd1;
+        vga_sync_n  <= 0;   // no sync on green
+        h_blank     <= 0;
+        v_blank     <= 0;
+        adr         <= 0;
+        v_advance   <= 0;
+    end else begin
+        h_counter <= h_counter + 10'd1;
+        case (h_counter)
+            000: h_blank <= 0;      // blank
+            039: h_blank <= 1;      // unblank
+            599: h_blank <= 0;      // blank
+            639: h_blank <= 0;      // hfront porch start
+            655: begin              // hfront porch end
+                vga_hs <= 0;        // hsync start
+                v_advance <= ~v_advance;
+                if (!v_counter[0] && v_blank)
+                    adr <= adr - 16'd280; // redo the line (line double)
+            end
+            751: vga_hs <= 1'd1;    // hback porch start
+            800: begin              // hback porch end
+                h_counter <= 0;
+                v_counter <= v_counter + 1;
+            end
+        endcase
+    
+        if ((h_blank & v_blank))
+            if (!h_counter[0])
+                adr <= adr + 1'd1;
+    
+        case (v_counter)
+            000: v_blank <= 0;      // blank
+            048: v_blank <= 1;      // unblank
+            432: v_blank <= 0;      // blank
+            480: begin              // vfront porch start
+                v_blank <= 0;       // disable RGB DACs
+                adr <= 0;
+            end
+            491: vga_vs <= 0;       // vfront porch end//vsync start
+            493: vga_vs <= 1'd1;    // vsync pulse end//vsync end
+            524: begin              // vback porch end
+                v_blank <= 1'd1;    // enable RGB DACs
+                v_counter <= 0;
+                v_advance <= 0;
+            end
+        endcase
+    
+        vga_r <= d[23:16];
+        vga_g <= d[15:8];
+        vga_b <= d[7:0];
+    end // if reset
+end // always
 endmodule
